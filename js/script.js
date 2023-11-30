a0849aed70301f4f7401d002a9a48758c5e8a91f
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2023-12-01 02:23:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
+const main = document.querySelector('.main');
 
 
 
@@ -8,6 +9,24 @@ if (burger){
         document.body.classList.toggle('lock');
         burger.classList.toggle('active');
         menu.classList.toggle('active');
+        main.classList.toggle('active');
     });
     
+}
+
+
+
+
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+for (let i = 0; i < accordionItems.length; i++) {
+  accordionItems[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    for (let j = 0; j < accordionItems.length; j++) {
+      if (accordionItems[j] !== this) {
+        accordionItems[j].classList.remove("active");
+      }
+    }
+  });
 }
